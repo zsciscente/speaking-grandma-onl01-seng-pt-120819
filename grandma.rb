a19c4ff73172635ae694866267bd018e887ddcdc
Zachary Sciscente <zsciscente@gmail.com>
2019-12-13 02:00:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,3 @@
-def speak_to_grandma (phrase)
-  if phrase == phrase.upcase
-  return "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN!"
-else 
-  return "HUH?! SPEAK UP, SONNY!"
+def speak_to_grandma(phrase)
+  
 end
